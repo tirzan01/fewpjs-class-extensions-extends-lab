c64f260071734a9598bb9e72ff7c473736de7230
thomas <thomasmonta98@gmail.com>
2021-06-19 21:16:12 +1000

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+class Polygon {
+    constructor (sideslength){
+        this.sideslength = [...sideslength]
+    }
+
+    get countSides (){
+        return this.sideslength.length
+    }
+
+    get perimeter (){
+        return this.sideslength.reduce((curr, acc) => curr + acc)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid (){
+        const sortSidesByLength = this.sideslength.sort((a, b) => a - b)
+        return sortSidesByLength[2] < sortSidesByLength[1] + sortSidesByLength[0]
+    }
+}
+
+class Square extends Polygon {
+    get isValid (){
+        return this.sideslength[0] === this.sideslength[1] && this.sideslength[2] === this.sideslength[3] && this.sideslength[1] == this.sideslength[2] 
+    }
+
+    get area (){
+        return parseInt(this.sideslength)**2
+    }
+}
